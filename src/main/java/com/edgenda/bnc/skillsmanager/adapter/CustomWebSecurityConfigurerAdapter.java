aef5e803521b2056ced5fcfb294323b508f903aa
unknown <BNC1213@formation.afiexpertise.com>
2019-05-27 10:05:44 -0400

    Clean-up class Custom

--- src/main/java/com/edgenda/bnc/skillsmanager/adapter/CustomWebSecurityConfigurerAdapter.java
+++ src/main/java/com/edgenda/bnc/skillsmanager/adapter/CustomWebSecurityConfigurerAdapter.java
@@ -1,8 +1,5 @@
 package com.edgenda.bnc.skillsmanager.adapter;
 
-import com.edgenda.bnc.skillsmanager.filter.CustomFilter;
-import com.edgenda.bnc.skillsmanager.service.MyBasicAuthenticationEntryPoint;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,9 +8,6 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
