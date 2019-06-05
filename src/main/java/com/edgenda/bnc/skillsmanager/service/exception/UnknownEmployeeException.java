e20ce5f932d6500fc7a40a1b95b4aaf8c98dde13
Gagnon Hane <gagnon.hane@bnc.ca>
2019-06-05 09:13:55 -0400

    code clean up

--- src/main/java/com/edgenda/bnc/skillsmanager/service/exception/UnknownEmployeeException.java
+++
@@ -1,9 +0,0 @@
-package com.edgenda.bnc.skillsmanager.service.exception;
-
-public class UnknownEmployeeException extends RuntimeException {
-
-    public UnknownEmployeeException(Long id) {
-        super("Unknown Employee with ID=" + id);
-    }
-
-}
